4ff51e3aff3c8b289f96dcee6fd50e286a5e9860
trendavi <dvid.trent@gmail.com>
2022-08-23 21:19:43 -0500

    fix bug in itype in debug.v

--- decode.v
+++ decode.v
@@ -20,23 +20,23 @@ module decode
    wire [4:0] rs2;
    
    always @ (*) begin
-      if ((ir_i & `DECODE_R_TYPE) == `DECODE_R_TYPE) begin
+      if (ir_i[6:0] == `DECODE_R_TYPE) begin
          ra_o <= rfile[ir_i[19:15]];
          rb_o <= rfile[ir_i[24:20]];
          itype_o <= `RTYPE; // Set R Type
       end
-      else if ((ir_i & `DECODE_I_TYPE) == `DECODE_I_TYPE) begin
+      else if (ir_i[6:0] == `DECODE_I_TYPE) begin
          ra_o <= rfile[ir_i[19:15]];
          rb_o <= ir_i[31:20];
          itype_o <= `ITYPE; // Set I Type
       end
-      else if ((ir_i & `DECODE_S_TYPE) == `DECODE_S_TYPE) begin
+      else if (ir_i[6:0] == `DECODE_S_TYPE) begin
          ra_o <= rfile[ir_i[19:15]];
          rb_o <= rfile[ir_i[24:20]];
          pass_o <= {ir_i[31:25], ir_i[11:7]};
          itype_o <= `STYPE; // Set S Type
       end
-      else if ((ir_i & `DECODE_U_TYPE) == `DECODE_U_TYPE) begin
+      else if (ir_i[6:0] == `DECODE_U_TYPE) begin
          pass_o <= ir_i[31:12];
          itype_o <= `UTYPE; // Set U Type
       end
--- opcode_masks.v
+++ opcode_masks.v
@@ -1,7 +1,7 @@
 // Register functions
 `define DECODE_R_TYPE 32'b0110011
 // Immediate 
-`define DECODE_I_TYPE 32'b0010011
+`define DECODE_I_TYPE 32'b0010111
 // Store
 `define DECODE_S_TYPE 32'b0100011
 // Upper Immediate
