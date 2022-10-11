35017a22cbf1634564d8b821d3ca2bbe88a9330a
trendavi <dvid.trent@gmail.com>
2022-10-10 21:12:06 -0500

    adi register test

--- tests/test.c
+++ tests/test.c
@@ -1,6 +1,19 @@
-int main () {
+#include "stdint.h"
 
-   // Exit Simulation
-   __asm__ ("lui x31, 0xdead");
-   return 0;
+void adi() {
+    register uint32_t a = 5;
+    register uint32_t b = 5;
+
+    if (a + b != 10) {
+        __asm__ ("lui x30, 0x1");
+        __asm__ ("lui x31, 0xdead");
+    }
+}
+
+int main () {
+    adi();
+
+    // Exit Simulation
+    __asm__ ("lui x31, 0xdead");
+    return 0;
 }
