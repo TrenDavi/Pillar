c65145f2b2d8f0eab082124aa9fdd87ca61290b6
trendavi <dvid.trent@gmail.com>
2022-10-11 21:02:44 -0500

    improve test function

--- tests/test.c
+++ tests/test.c
@@ -1,17 +1,20 @@
 #include "stdint.h"
 
-void adi() {
-    register uint32_t a = 5;
-    register uint32_t b = 5;
-
-    if (a + b != 10) {
-        __asm__ ("lui x30, 0x1");
-        __asm__ ("lui x31, 0xdead");
-    }
+void addi() {
+    __asm__ ("li x3, 0x0");
+    __asm__ ("addi x3, x3, 0x1");
+    __asm__ ("addi x3, x3, 0x2");
+    __asm__ ("addi x3, x3, 0x3");
+    __asm__ ("li x4, 0x6");
+    __asm__ ("bne x3, x4, exitfail");
+    __asm__ ("j exit");
+    __asm__ ("exitfail:");
+    __asm__ ("lui x31, 0xdead");
+    __asm__ ("exit:");
 }
 
 int main () {
-    adi();
+    addi();
 
     // Exit Simulation
     __asm__ ("lui x31, 0xdead");
