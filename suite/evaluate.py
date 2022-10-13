d1edd155dc0c04879b78b76518ae8fa8f0c42318
trendavi <dvid.trent@gmail.com>
2022-10-13 16:48:44 -0500

    add function structure

--- suite/evaluate.py
+++ suite/evaluate.py
@@ -1,2 +1,35 @@
+#!/usr/bin/env python3
+
+import subprocess
+
+COMPILE_COMMAND = [     \
+    "ls",               \
+]
+
+HEX_COMMAND = [         \
+    "./elf2hex",        \
+    "--bit-width",      \
+    "8",                \
+    "--input",          \
+    "suite.o",          \
+    "--output",         \
+    "tmp.hex",          \
+]
+
+TEST_COMMAND = [        \
+    "ls",               \
+]
+
+def main(): 
+    # Compile
+    subprocess.run(COMPILE_COMMAND)
+
+    # Hex
+    subprocess.run(HEX_COMMAND)
+
+    #Test
+    subprocess.run(TEST_COMMAND)
+
+
 if __name__ == "__main__":
-    print("hello")
+    main()
